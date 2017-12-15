b523d6f8661bc87a5d3aea70ee220383516b1e4f
JeffRisberg <jrisberg@aol.com>
2017-12-15 11:41:24 -0800

    updated store

--- app/components/ItemForm.vue
+++ app/components/ItemForm.vue
@@ -6,15 +6,15 @@
 </template>
 
 <script>
-    export default{
-        mounted: function() {
-            console.log("ItemForm Mounted");
-            this.$store.dispatch('fetchItem');
-        },
-        computed: {
-            events() {
-                return this.$store.getters.items
-            }
-        }
+  export default {
+    mounted: function () {
+      console.log("ItemForm Mounted");
+      this.$store.dispatch('fetchItem');
+    },
+    computed: {
+      events() {
+        return this.$store.getters.items
+      }
     }
+  }
 </script>
--- app/vuex/store.js
+++ app/vuex/store.js
@@ -5,116 +5,120 @@ import fetch from 'isomorphic-fetch';
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-    state: {
-        events: [],
-        items: []
+  state: {
+    events: [],
+    items: []
+  },
+  mutations: {
+    FETCH_EVENTS(state, events) {
+      state.events = events;
+      // set the wait flag
     },
-    mutations: {
-        FETCH_EVENTS(state, events) {
-            state.events = events;
-            // set the wait flag
-        },
-        FETCH_EVENTS_SUCCESS(state) {
-            // clear the wait flag
-        },
-        FETCH_ITEMS(state, items) {
-            state.items = items;
-            // set the wait flag
-        },
-        FETCH_ITEMS_SUCCESS(state) {
-            // clear the wait flag
-        },
+    FETCH_EVENTS_SUCCESS(state) {
+      // clear the wait flag
     },
+    FETCH_ITEMS(state, items) {
+      state.items = items;
+      // set the wait flag
+    },
+    FETCH_ITEMS_SUCCESS(state) {
+      // clear the wait flag
+    },
+  },
 
-    actions: {
-        fetchEvents({commit}) {
-            // async call here
-            fetch('/api/events', {})
-                  .then(response => response.json())
-                  .then((json) => {
-                    commit('FETCH_EVENTS', json.data);
-                  });
-        },
-        saveEvent({commit}, event) {
-            fetch('/api/events/' + event.id, {
-              method: 'PUT',
-              headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-              },
-              body: JSON.stringify({ event: event })
-            })
-              .then(response => response.json())
-              .then((json) => {
-                commit('PERSIST_EVENT_SUCCESS',
-                  events: [json.data]
-                });
-                //dispatch(push('/events'));
-              });
-          };
-        },
-        addEvent({commit}, event) {
-            fetch('/api/events/' + event.id, {
-                      method: 'POST',
-                      headers: {
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json'
-                      },
-                      body: JSON.stringify({ event: event })
-                    })
-                      .then(response => response.json())
-                      .then((json) => {
-                        commit('PERSIST_EVENT_SUCCESS',
-                          events: [json.data]
-                        });
-                        //dispatch(push('/events'));
-                      });
-                  };
-        },
-        deleteEvent({commit}, event) {
-            fetch('/api/events/' + event.id, {
-              method: 'DELETE',
-              headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-              },
-              body: JSON.stringify({ event: event })
-            })
-              .then(response => response.json())
-              .then((json) => {
-                commit('PERSIST_EVENT_SUCCESS',
-                  events: [json.data]
-                });
-                //dispatch(push('/events'));
-              });
-          };
-        },
-        fetchItems({commit}) {
-            // async call here
-            fetch('/api/items', {})
-                  .then(response => response.json())
-                  .then((json) => {
-                    commit('FETCH_ITEMS', json.data);
-                  });
-        },
-        saveItem({commit}, item) {
-            // async call here
-            fetch('/api/items', {
-             method: 'PUT'
-            })
-                  .then(response => response.json())
-                  .then((json) => {
-                    commit('FETCH_ITEMS', json.data);
-                  });
-        },
-         addItem({commit}, item) {
-         },
-         deleteItem({commit}, item) {
-         }
+  actions: {
+    fetchEvents({ commit }) {
+      // async call here
+      fetch('/api/events', {})
+        .then(response => response.json())
+        .then((json) => {
+          commit('FETCH_EVENTS', json.data);
+        })
     },
-
-    getters: {
-        events: state => state.events,
-        items: state => state.items
+    saveEvent({ commit }, event) {
+      fetch('/api/events', {
+        method: 'PUT',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ event: event })
+      })
+        .then(response => response.json())
+        .then((json) => {
+          commit('PERSIST_EVENT_SUCCESS', json.data)
+        })
+    },
+    addEvent({ commit }, event) {
+      fetch('/api/events', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ event: event })
+      })
+        .then(response => response.json())
+        .then((json) => {
+          commit('PERSIST_EVENT_SUCCESS', json.data)
+        })
+    },
+    deleteEvent({ commit }, event) {
+      fetch('/api/events/' + event.id, {
+        method: 'DELETE',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ event: event })
+      })
+    },
+    fetchItems({ commit }) {
+      // async call here
+      fetch('/api/items', {})
+        .then(response => response.json())
+        .then((json) => {
+          commit('FETCH_ITEMS', json.data);
+        })
+    },
+    saveItem({ commit }, item) {
+      // async call here
+      fetch('/api/items', {
+        method: 'PUT'
+      })
+        .then(response => response.json())
+        .then((json) => {
+          commit('FETCH_ITEMS', json.data);
+        })
+    },
+    addItem({ commit }, item) {
+      fetch('/api/items', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ item: item })
+      })
+        .then(response => response.json())
+        .then((json) => {
+          commit('PERSIST_ITEM_SUCCESS', json.data)
+        })
+    },
+    deleteItem({ commit }, item) {
+      fetch('/api/items/' + item.id, {
+        method: 'DELETE',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ item: item })
+      })
     }
+  },
+
+  getters: {
+    events: state => state.events,
+    items: state => state.items
+  }
 })
